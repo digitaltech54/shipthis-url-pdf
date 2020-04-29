28924dcc5d85e1287b3e764200187493df2c167b
Ahmed <reachshabib@gmail.com>
2020-04-29 17:42:24 +0530

    Added some wait time

--- app.js
+++ app.js
@@ -84,14 +84,15 @@ app.use(async (req, res) => {
         if (!requestBody.url) {
             return res.send('Please provide valid URL in post parameter');
         } else {
-            await page.goto(requestBody.url, {waitUntil: ['networkidle0', 'load', 'domcontentloaded','networkidle2']});
+            await page.goto(requestBody.url, {waitUntil: ['networkidle0', 'load', 'domcontentloaded', 'networkidle2']});
             await page.waitFor(1000);
         }
     } else if (requestBody.type === 'html') {
         if (!requestBody.content) {
             return res.send('Please provide valid html content in post parameter');
         } else {
-            await page.goto(`data:text/html, <html content> <meta charset="utf-8">${requestBody.content}`, { waitUntil: ['networkidle0', 'load', 'domcontentloaded'] });
+            await page.goto(`data:text/html, <html content> <meta charset="utf-8">${requestBody.content}`, {waitUntil: ['networkidle0', 'load', 'domcontentloaded', 'networkidle2']});
+            await page.waitFor(100);
         }
     } else {
         return res.send('type is not a valid parameter');
